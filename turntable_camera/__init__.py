d8215514a4c59cd06013c45b1ec1a7fb8bbf2b80
char8294 <char8294@outlook.com>
2026-06-14 00:02:34 +0700

    1.1.4

--- turntable_camera/__init__.py
+++ turntable_camera/__init__.py
@@ -19,7 +19,7 @@ from bpy.types import Panel, Operator, PropertyGroup, UIList
 bl_info = {
     "name": "Turntable Camera",
     "author": "TEERA",
-    "version": (1, 1, 3),
+    "version": (1, 1, 4),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar > Turntable Tab",
     "description": "Turntable animation สำหรับโมเดล: กล้องหมุนรอบโมเดล หรือโมเดลหมุนบนที่ พร้อมพรีเซ็ตกล้องสำเร็จรูป",
